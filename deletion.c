ab10149a289dc9aa3eec6018047ac08341442eb0
a-nexus <anuragsingh77077@gmail.com>
2026-02-10 21:33:50 +0530

    DELETION OF THE ARRAY OF THE LAST ELEMENT

--- deletion.c
+++ deletion.c
@@ -1,23 +1,45 @@
 //HERE IN THIS DIRECTORY I PERFERMED ALL THE DELETION IN THE ARRAY
 //FIRST IN THE DELETING IN THE END
 
+// #include <stdio.h>
+// #include <conio.h>
+
+// int main(){
+//     int arr[100],num,item;
+//     printf("ENTER THE SIZE OF THE ARRAY:");
+//     scanf("%d",&num);
+//     for(int i=0;i<num;i++){
+//         printf("%d INDEXED VALUE IS:",i+1);
+//         scanf("%d",&arr[i]);
+//     }
+//     for(int i=0;i<num;i++){
+//        arr[i]=arr[i+1];  //HERE IS THE CONCEPT BETWEEN THE DELETION OF THE FIRST ELEMENT
+//     }
+    
+//     num--;//IF WE DONT USE N-- THEN THE FIRST ELEMENT WHICH WE DELETE FROM THE ARRAY IT COME BACK IN THE LAST
+//     for(int i=0;i<num;i++){
+//        printf("%d\t",arr[i]);
+//     }
+// }
+
+
+//NEST DELETION AT THE ENDING 
+//IN THAT DELETION THE CONCEPT IS VERY SIMPLE JUST YOU HAVE TO DEDUCT THE INPUT AFTER TAKING DEDUCT BY 1 
+
 #include <stdio.h>
 #include <conio.h>
-
+ 
 int main(){
-    int arr[100],num,item;
+    int arr[100],num;
     printf("ENTER THE SIZE OF THE ARRAY:");
     scanf("%d",&num);
     for(int i=0;i<num;i++){
-        printf("%d INDEXED VALUE IS:",i+1);
+        printf("%d indexed value=",i+1);
         scanf("%d",&arr[i]);
     }
+    num--;//WE DELETED THE LAST STORAGE BOX BY NUM-- SO THE LAST INDEX VALUE IS NOT PRINTING AS WELL SO WE DON IT;
     for(int i=0;i<num;i++){
-       arr[i]=arr[i+1];  //HERE IS THE CONCEPT BETWEEN THE DELETION OF THE FIRST ELEMENT
+        printf("%d\t",arr[i]);
     }
-    
-    num--;//IF WE DONT USE N-- THEN THE FIRST ELEMENT WHICH WE DELETE FROM THE ARRAY IT COME BACK IN THE LAST
-    for(int i=0;i<num;i++){
-       printf("%d\t",arr[i]);
-    }
+return 0;
 }
