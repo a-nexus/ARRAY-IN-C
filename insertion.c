4802016b68df290f5743158c97b295b5787d1012
a-nexus <anuragsingh77077@gmail.com>
2026-02-10 19:29:59 +0530

    INSERTION AT THE SPECIFIC POSITION

--- insertion.c
+++ insertion.c
@@ -31,23 +31,54 @@
 //NEXT INSERTION
 //ADDING THE VALUE AT THE END OF THE ARRAY 
 
+// #include <stdio.h>
+// #include <conio.h>
+
+// int main(){
+//     int arr[100];
+//     int n,num;
+//     printf("ENTER THE SIZE OF THE ARRAY:");
+//     scanf("%d",&n);
+//     for(int i=0;i<n;i++){
+//         printf("%d:",i+1);
+//         scanf("%d",&arr[i]);
+//     }
+//     printf("ENTER THE NUMBER YOU WANT TO ADD IN THE END:");
+//     scanf("%d",&num);
+//     arr[n]=num;
+//     n++;
+//     for(int i=0;i<n;i++){
+//         printf("%d\t",arr[i]);
+//     }
+//     return 0;
+// }
+
+
+//NEXT AND LAST INSERTION
+//AT AN SPECIFIC POSITION
+
 #include <stdio.h>
 #include <conio.h>
 
 int main(){
-    int arr[100];
-    int n,num;
+    int arr[100],num,item,n;
     printf("ENTER THE SIZE OF THE ARRAY:");
-    scanf("%d",&n);
-    for(int i=0;i<n;i++){
-        printf("%d:",i+1);
+    scanf("%d",&num);
+    for(int i=0;i<num;i++){
+        printf("%d INDEX VALUE:",i+1);
         scanf("%d",&arr[i]);
     }
-    printf("ENTER THE NUMBER YOU WANT TO ADD IN THE END:");
-    scanf("%d",&num);
-    arr[n]=num;
-    n++;
-    for(int i=0;i<n;i++){
+    printf("ENTER THE POSITION WHERE YOU WANT TO ADD THE ELEMENT:");
+    scanf("%d",&n);
+    for(int i=num-1;i>=n-1;i--){
+        arr[i+1]=arr[i];
+    }
+    printf("ENTER THE VALUE YOU HAVE TO INSERT IN THE SPECIFIC POSITION:");
+    scanf("%d",&item);
+    
+    arr[n-1]=item;
+     num++;
+    for(int i=0;i<=num-1;i++){
         printf("%d\t",arr[i]);
     }
     return 0;
