fc09d984f24540974e68e85e17c3164a06e31099
a-nexus <anuragsingh77077@gmail.com>
2026-02-21 23:15:04 +0530

    INSERTION SHORTING

--- shorting.c
+++ shorting.c
@@ -28,30 +28,77 @@
 
 //SLECTION SHORTING
 
+// #include <stdio.h>
+
+// int main(){
+//     int num,temp;
+//     printf("ENTER THE ELEMENT OF THE ARRAY:");
+//     scanf("%d",&num);
+//     int arr[num];
+//     for(int i=0;i<num;i++){
+//         printf("%d indexed value=",i+1);
+//         scanf("%d",&arr[i]);
+//     }
+//     for(int i=0;i<num;i++){
+//         int smallest=i;
+//         for(int j=i+1;j<num;j++){
+//             if(arr[j]<arr[smallest]){
+//                smallest=j;
+//             }
+//         }
+//           temp=arr[i];
+//                 arr[i]=arr[smallest];
+//                 arr[smallest]=temp;
+//     }
+//     for(int i=0;i<num;i++){
+//         printf("%d\t",arr[i]);
+//     }
+//     return 0;
+// }
+
+//INSERTION SHORTING
+
 #include <stdio.h>
 
 int main(){
-    int num,temp;
-    printf("ENTER THE ELEMENT OF THE ARRAY:");
+    int temp,num,arr[100],tar;
+    printf("ENTER THE NUMBER OF ELEMENT=");
     scanf("%d",&num);
-    int arr[num];
+    if(num>=100){
+        printf("INVALID INPUT");
+        return 0;
+    }
+    printf("ENTER YOUR TARGET NUMBER:");
+    scanf("%d",&tar);
+    
     for(int i=0;i<num;i++){
         printf("%d indexed value=",i+1);
         scanf("%d",&arr[i]);
     }
-    for(int i=0;i<num;i++){
-        int smallest=i;
+    for(int i=0;i<num-1;i++){
+        int smaller=i;
         for(int j=i+1;j<num;j++){
-            if(arr[j]<arr[smallest]){
-               smallest=j;
-            }
+            if(arr[j]<arr[smaller]){
+            smaller=j;
         }
-          temp=arr[i];
-                arr[i]=arr[smallest];
-                arr[smallest]=temp;
     }
-    for(int i=0;i<num;i++){
-        printf("%d\t",arr[i]);
-    }
-    return 0;
+    temp=arr[smaller];
+    arr[smaller]=arr[i];
+    arr[i]=temp;
+
+}
+int i;
+
+for( i=num-1;i>=0 && arr[i]>tar;i--){
+    
+       arr[i+1]=arr[i];  
+    
+   
+}
+arr[i+1]=tar;
+num++;
+for(int i=0;i<num;i++){
+    printf("%d\t",arr[i]);
+}
+return 0;
 }
